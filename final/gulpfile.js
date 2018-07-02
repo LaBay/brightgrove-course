5f4bc3ceb76f769b7ab23b4168b9c6708e2f4247
“LaBay” <labay@i.ua>
2018-07-03 00:26:06 +0300

    +

--- final/gulpfile.js
+++ final/gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./style/sass/style.scss')
+  return gulp.src('./style/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('../style/build'));
+    .pipe(gulp.dest('../style/build/style.css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./style/sass/style.scss',  ['sass']);
+  gulp.watch('./style/sass/*.scss',  ['sass']);
 });
