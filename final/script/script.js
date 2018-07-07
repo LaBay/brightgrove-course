94f659c98aabd6fcab0da3b236066618218872f7
“LaBay” <labay@i.ua>
2018-07-08 02:07:29 +0300

    New files have been added

--- final/script/script.js
+++ final/script/script.js
@@ -1,1 +1,13 @@
 "use strict"
+
+/* TRIGGER OF FOURTH SECTION "SERVICES WE PROVIDE" */
+
+$('.services-trigger-wrapper div').click(function(){
+	let $i = $('.services-trigger-wrapper div').index(this);
+	console.log($i);
+	let $elements = $('.services-item-wrapper div');
+	$elements.each(function(){
+		if($($elements).index(this)== $i) $(this).addClass('active')
+			else $(this).removeClass('active')
+	})
+})
