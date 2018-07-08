c4275622193f10b8e5ef96f5229f5ab81c8f6dbe
“LaBay” <labay@i.ua>
2018-07-09 01:38:48 +0300

    Changes in some files. New files have been added.

--- final/script/script.js
+++ final/script/script.js
@@ -4,10 +4,23 @@
 
 $('.services-trigger-wrapper div').click(function(){
 	let $i = $('.services-trigger-wrapper div').index(this);
-	console.log($i);
 	let $elements = $('.services-item-wrapper div');
+	console.log($elements.length);
 	$elements.each(function(){
 		if($($elements).index(this)== $i) $(this).addClass('active')
 			else $(this).removeClass('active')
 	})
 })
+
+
+/* SLIDER */
+
+$(document).ready(function(){
+  $('.slider').slick({
+  	dots: true,
+  	slidesToShow: 2,
+  	slidesToScroll: 1,
+  	prevArrow: '<i class="fa fa-chevron-left slider-chevron" id="left-direction">',
+  	nextArrow: '<i class="fa fa-chevron-right slider-chevron" id="right-direction"></i>',
+  });
+});
